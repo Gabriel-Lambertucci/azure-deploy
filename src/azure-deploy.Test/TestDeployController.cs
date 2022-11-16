ebd2d8c8b328fd99f71838c0e5e12f1768499699
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-11-16 08:37:03 -0300

    Initial commit

---
+++ src/azure-deploy.Test/TestDeployController.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+
+namespace AzureDeploy.Test;
+
+public class TestDeployController
+{
+    /// <summary>
+    /// This function is to test the api controller requests
+    /// </summary>
+    [Fact]
+    public void TestGetStatusCodeSuccess()
+    {
+        throw new NotImplementedException();
+    }
+}
